f772a7999884c2874568675049563b89044f8017
lelandlopez <lelandlopez@gmail.com>
2017-08-18 16:10:15 -1000

    added rule page

--- frontend/src/components/Rule.vue
+++ frontend/src/components/Rule.vue
@@ -1,39 +1,32 @@
 <template>
   <div class="container">
-    <md-toolbar>
-      <md-button class="md-icon-button">
-        <md-icon></md-icon>
-      </md-button>
-
-      <h2 class="md-title" style="flex: 1">{{rule.name}}</h2>
-    </md-toolbar>
     <div class="main-content">
       <div class="rule-name">
         <h2>{{rule.name}}</h2>
+        <div class="status-sign">
+          <md-avatar class="md-avatar-icon md-large">
+            <img v-if="rule.status == 0" src="../assets/greenbox.png" alt="Avatar">
+            <img v-if="rule.status == 1" src="../assets/greenbox.png" alt="Avatar">
+            <img v-if="rule.status == 2" src="../assets/greenbox.png" alt="Avatar">
+          </md-avatar>
+        </div>
       </div>
       <div class="bill-text">
         <div class="title">
-          <h4>Bill Text</h4>
+            <h4>Bill Text</h4>
+          </div>
+          <div class="description">
+            <p>{{ rule.description }}</p>
+            <div v-for="(time, index) in rule.time">
+              ({{index + 1}}) <b>{{time.name}}</b> {{time.description}}
+            </div>
+          </div>
         </div>
-        <div class="description">
-          <p>
-          No person shall sit or lie on a public mall, or on a tarp, towel, sheet, blanket,
-          sleeping bag, bedding, planter, chair, bench, or any other object or material
-          placed upon a public mall during the following hours:
-          </p>
-          <p>
-          (1) <b>Fort Street Mall</b>. Monday to Friday from 5:00 a.m. to 7:00 p.m.<br>
-          (2) <b>Kekaulike Mall</b>. Monday to Sunday from 5:00 am. to 7:00 p.m.<br>
-          (3) <b>Sun Yat Sen Mall</b>. Monday to Friday from 5:00 a.m. to 7:00 p.m.<br>
-          (4) <b>Union Mall</b>. Monday to Friday from 5:00 a.m. to 7:00 p.m.<br>
-          </p>
+        <div class="ordiance">
+          <div class="title">
+            <h4>Ordiance</h4>
+          </div>
         </div>
-      </div>
-      <div class="ordiance">
-        <div class="title">
-          <h4>Ordiance</h4>
-        </div>
-      </div>
     </div>
   </div>
 
@@ -44,13 +37,33 @@ export default {
   name: 'Rule',
   data () {
     return {
+      // static rules *remove when api is created
       rule: {
-        name: 'Sit Lie Ban'
+        name: 'Sit Lie Ban',
+        status: 0,
+        description: 'No person shall sit or lie on a public mall, or on a tarp, towel, sheet, blanket,' +
+          'sleeping bag, bedding, planter, chair, bench, or any other object or material' +
+          'placed upon a public mall during the following hours:',
+        time: [
+          {
+            name: 'Fort Street Mall',
+            description: 'Monday to Sunday from 5:00 am. to 7:00 p.m.'
+          }, {
+            name: 'Kekaulike Mall',
+            description: 'Monday to Friday from 5:00 a.m. to 7:00 p.m.'
+          }, {
+            name: 'Sun Yat Sen Mall',
+            description: 'Monday to Friday from 5:00 a.m. to 7:00 p.m.'
+          }, {
+            name: 'Union Mall',
+            description: 'Monday to Friday from 5:00 a.m. to 7:00 p.m.'
+          }
+        ]
       }
     }
   },
   mounted () {
-    console.log(this.$route.params.ruleId)
+    // use API to grab rule and set it here with this.$route.params.ruleId
   }
 }
 </script>
@@ -79,6 +92,20 @@ export default {
   }
   .description > p {
     margin: 0px;
+    padding-bottom: 20px;
+  }
+  .rule-name > h2 {
+    display: inline-block;
+    width: 75%;
+  }
+  .status-sign {
+    display: inline-block;
+  }
+  .rule-name {
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+  .bill-text {
     padding-bottom: 20px;
   }
 
