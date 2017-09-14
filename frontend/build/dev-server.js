2cb9618af513dd4372290e316ad1c951f5288b51
McKay Davis <mckay.davis@gmail.com>
2017-09-13 19:48:54 -1000

    Remove polling, it's too taxing and locking vagrant

--- frontend/build/dev-server.js
+++ frontend/build/dev-server.js
@@ -26,10 +26,6 @@ var app = express()
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
-  watchOptions: {
-     poll: 1000,
-     aggregateTimeout: 1000
-  },
   publicPath: webpackConfig.output.publicPath,
   quiet: true
 })
