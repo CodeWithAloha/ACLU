4133864cb95de71a6f478d7b91a31f7267b51c6b
Pipe Martina <felimartina@outlook.com>
2017-10-04 19:37:25 -1000

    Updated `after_install.sh` file to point to the right folder for docker-compose

--- scripts/aws-code-deploy/after_install.sh
+++ scripts/aws-code-deploy/after_install.sh
@@ -4,8 +4,10 @@ yarn
 yarn build
 
 # Docker compose
-cd /var/project-aclu/backend
+cd /var/project-aclu/
 docker-compose build
 
 # Create the spatial index
+# TODO - Not sure if it is ok to run this on every deploy...is it idempotent?
+# TODO2 - What about import scripts?
 docker exec -it $(docker ps -aqf "name=aclu-db") mongo aclu --eval "db.features.ensureIndex({'geojson.geometry': '2dsphere'})"
