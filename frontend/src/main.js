cf96b4f327efa068faaf8054979711cbbf6693a4
rbvea <russellbvea@gmail.com>
2017-10-10 22:00:15 -1000

    Using Vuex, adding stop signs, active states.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,36 +1,38 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
+import Vue from "vue";
 
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.css'
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.css";
 
-import MaterialDesignsIcons from 'material-design-icons'
-import 'material-design-icons/iconfont/material-icons.css'
+import MaterialDesignsIcons from "material-design-icons";
+import "material-design-icons/iconfont/material-icons.css";
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-default/index.css";
 
-import locale from 'element-ui/lib/locale/lang/en'
+import locale from "element-ui/lib/locale/lang/en";
 
-import App from './App'
-import router from './router'
-import store from './store'
+import App from "./App";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(VueMaterial)
-Vue.use(MaterialDesignsIcons)
+Vue.use(VueMaterial);
+Vue.use(MaterialDesignsIcons);
+
+import "colors.css/src/_variables.css";
 
 /* eslint-disable no-new */
 
-Vue.use(VueMaterial)
-Vue.use(ElementUI, { locale })
+Vue.use(VueMaterial);
+Vue.use(ElementUI, { locale });
 
 new Vue({
-  el: '#app',
+  el: "#app",
   store,
   router,
-  template: '<App/>',
+  template: "<App/>",
   components: { App }
-})
+});
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,18 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
     locationDetermined: false,
-    location: null
+    location: null,
+    rules: []
   },
   mutations: {
-    locationFound (state, coords) {
-      state.locationDetermined = true
-      state.location = [coords.latitude, coords.longitude]
+    locationFound(state, coords) {
+      state.locationDetermined = true;
+      state.location = [coords.latitude, coords.longitude];
+    },
+    updateRules(state, rules) {
+      state.rules = rules;
     }
   }
-})
+});
 
-export default store
+export default store;
