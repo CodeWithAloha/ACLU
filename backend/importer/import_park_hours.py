9b135b77555b9cd419b3bc7b88cb227aef1be2b2
McKay Davis <mckay.davis@gmail.com>
2017-10-18 20:08:52 -1000

    Parse open and close hours. Keep all entires local to park. dump JSON to stdout

--- backend/importer/import_park_hours.py
+++ backend/importer/import_park_hours.py
@@ -10,6 +10,7 @@ import os
 import logging
 import logging.config
 import requests
+import json
 from bs4 import BeautifulSoup
 
 logging.config.fileConfig(
@@ -29,36 +30,38 @@ def import_park_hours():
     """Import park hours"""
     page = requests.get(PARK_URL)
     soup = BeautifulSoup(page.text, "html.parser")
-    mapping = {}
+    mapping = []
+    NAME = "name"
+    OPEN = "open"
+    HOURS = "hours"
+    CLOSE = "close"
     for table in soup('table'):
         # skip table heading
         for tr in table('tr')[1:]:
             cells = tr('td')
             park = cells[0]('span')
             times = cells[1]('span')
-            num_park_spans = len(park)
-            num_times_spans = len(times)
+            park = [e.text.strip() for e in park]
+            times = [e.text.strip() for e in times]
+            entry = {}
+            while len(park) > len(times):
+                times = [""] + times
+            while len(times) > len(park):
+                park.append(park[-1])
 
-            if num_park_spans == num_times_spans:
-                for p, t in zip(park, times):
-                    mapping[p.text.strip()] = t.text.strip()
-            elif num_park_spans > num_times_spans:
-                name = park[0].text.strip()
-                for index, span in enumerate(park[1:]):
-                    if index < num_times_spans and index < 1:
-                        val = times[index].text.strip()
-                mapping[name] = val
-            elif num_park_spans < num_times_spans:
-                for index, span in enumerate(times[:1]):
-                    if index < num_park_spans:
-                        # name
-                        name = park[index].text.strip()
-                    # strip out Winter hours for now
-                    span.text.strip()
-                val = times[num_times_spans - 1].text.strip()
-                mapping[name] = val.replace('(Winter)', '')
+            entry[NAME] = park[0]
+            hours = {}
+            for p, t in zip(park, times):
+                if " to " in t:
+                    t = t.split(" to ")
+                else:
+                    t = t.split('-')
+                t = [e.strip() for e in t]
+                hours[p] = t
+            entry['hours'] = hours
+            mapping.append(entry)
     return mapping
 
 
 if __name__ == '__main__':
-    import_park_hours()
+    print(json.dumps(import_park_hours()))
