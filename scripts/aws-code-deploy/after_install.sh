6d1482d3f924f5509e2e08c46a5cc4be87b8ba39
Pipe Martina <felimartina@outlook.com>
2017-10-19 10:54:34 -1000

    Dockers commands don't need to run with sudo

--- scripts/aws-code-deploy/after_install.sh
+++ scripts/aws-code-deploy/after_install.sh
@@ -8,12 +8,12 @@ export IMAGES_REPO_URL=$IMAGES_REPO_URL
 
 # Upgrade aws cli to support --no-include-email for docker login (email was deprecated from docker)
 pip install --upgrade awscli
-sudo $(aws ecr get-login --region $ECR_REGION --no-include-email)
+$(aws ecr get-login --region $ECR_REGION --no-include-email)
 
 # Docker compose
 cd /var/project-aclu/
-sudo docker-compose down
-sudo docker-compose up -d
+docker-compose down
+docker-compose up -d
 
 # Create the spatial index TODO - Not sure if it is ok to run this on every deploy...is it idempotent? TODO2 - What about import scripts?
-sudo docker exec $(docker ps -aqf "name=aclu-db") mongo aclu --eval "db.features.ensureIndex({'geojson.geometry': '2dsphere'})"
+docker exec $(docker ps -aqf "name=aclu-db") mongo aclu --eval "db.features.ensureIndex({'geojson.geometry': '2dsphere'})"
