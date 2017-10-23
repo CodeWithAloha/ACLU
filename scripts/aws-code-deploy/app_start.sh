b3576b999d2918b0f3bf5d7a2a8ddb84ab55a33a
Pipe Martina <felimartina@outlook.com>
2017-10-23 08:50:58 -1000

    Added code to remove existing `2017-07-19.parks.geojson` to work around error `ERROR 6: The GeoJSON driver does not overwrite existing files.`

--- scripts/aws-code-deploy/app_start.sh
+++ scripts/aws-code-deploy/app_start.sh
@@ -6,7 +6,10 @@ sudo docker-compose up -d
 # Create the spatial index
 docker exec $(docker ps -aqf "name=aclu-db") mongo aclu --eval "db.features.ensureIndex({'geojson.geometry': '2dsphere'})"
 
-# seed db. We do this on every run now because we are on a stage site!
-cd backend/etc
+# Cleanup previous run of the db feed
+cd backend
+sudo rm -f importer/2017-07-19.parks.geojson
+# Seed db. We do this on every run now because we are on a stage site!
+cd etc
 sudo chmod +x seed_fake_park_data.sh
 ./seed_fake_park_data.sh
