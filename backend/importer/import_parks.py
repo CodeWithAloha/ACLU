c5ce0823c0e38902c0746ab085e5d809a1049ad6
Ryan <ryankanno@localkinegrinds.com>
2018-01-10 19:16:17 -1000

    Refactored import_parks.py

--- backend/importer/import_parks.py
+++ backend/importer/import_parks.py
@@ -18,59 +18,62 @@ import uuid
 
 logging.config.fileConfig(
     os.path.join(os.path.dirname(os.path.realpath(__file__)), 'logging.conf'))
-logger = logging.getLogger("aclu_importer.features")
-
-
-API_BASE_URL = "http://localhost:50050"
-API_BASE_URL_FORMAT = "{0}/{{0}}".format(API_BASE_URL)
+logger = logging.getLogger("aclu_importer.parks")
 
 
 @click.command()
-@click.option('--feature_collection_path', default=None, help='Path to feature collection file being imported.')
-def import_features(feature_collection_path=None):
-    # Let's just hardcode one implementation and we'll worry about the
-    # abstraction later.
-    #
-    # I think what needs to be done here is that we use a template file (via
-    # Jinja) that we feed into this script. The template file has the 'default'
-    # properties aka the holes we need to fill via lambdas and it's basically
-    # used to complete the feature for submission. I imagine different
-    # feature/geojson files will each need a unique template (and it should
-    # scale somewhat nicely)
+@click.option('--park_features_path', help='Path to park features file being imported.', required=True, type=click.Path(exists=True))
+@click.option('--api_base_url', default='http://localhost:50050', help='API base url. Defaults to http://localhost:50050')
+def import_park_features(park_features_path, api_base_url):
 
-    with open("../data/parks/2017.10.20_Honolulu-Park-Hours/park-closure-hours.json") as parks_file:
-        park_hours = json.load(parks_file)
+    organization = _get_organization(api_base_url, "Park")
 
-        logger.info(park_hours)
+    if organization:
 
-        if feature_collection_path and os.path.isfile(os.path.realpath(feature_collection_path)):
-            organization = _get_organization("Park")
+        park_hours = _get_park_hours("../data/parks/2017.10.20_Honolulu-Park-Hours/park-closure-hours.json")
 
-            if organization:
-                for feature in _features_from_path(os.path.realpath(feature_collection_path)):
-                    f = {
-                        "_id": str(uuid.uuid4()),
-                        "geojson": feature,
-                        "restrictions": {},
-                        "organization": organization["_id"],
-                        "name": feature['properties']['name'],
-                        "last_imported_at": datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
-                    }
-                    hours = park_hours.get(
-                        feature['properties']['name'], False)
-                    if hours:
-                        try:
-                            hours_parts = hours.get('park')
-                            f['restrictions']['hours_start'] = int(hours_parts.get( 'open'))
-                            f['restrictions']['hours_end'] = int(hours_parts.get('close'))
-                        except:
-                            pass
-                    _post_features(f)
-            return sys.exit(0)
-        else:
-            logger.error(
-                "Please input a valid, importable feature collection file.")
-            return sys.exit(100)
+        for feature in _features_from_path(park_features_path):
+            f = _construct_park_feature_json(feature, organization, park_hours)
+            _post_features(api_base_url, f)
+
+    return sys.exit(0)
+
+
+def _construct_park_feature_json(feature, organization, park_hours=None):
+    park_name = feature['properties']['name']
+    f = {
+        "_id": str(uuid.uuid4()),
+        "geojson": feature,
+        "restrictions": {},
+        "organization": organization["_id"],
+        "name": park_name,
+        "last_imported_at": datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    }
+
+    try:
+        hours = park_hours.get(park_name, False)
+        if hours:
+            logger.info("Setting park hours for {0}".format(park_name))
+            hours_parts = hours.get('park')
+            f['restrictions']['hours_start'] = int(hours_parts.get('open'))
+            f['restrictions']['hours_end'] = int(hours_parts.get('close'))
+    except:
+        pass
+
+    return f
+
+
+def _get_park_hours(park_hours_path=None):
+    try:
+        with open(park_hours_path) as parks_file:
+            return json.load(parks_file)
+    except:
+        logger.error("Error occurred trying to retrieve park hours.")
+        return None
+
+
+def _get_api_resource_url(api_base_url, resource):
+    return "{0}/{1}".format(api_base_url, resource)
 
 
 def _features_from_path(feature_collection_path=None):
@@ -81,8 +84,8 @@ def _features_from_path(feature_collection_path=None):
                 yield feature
 
 
-def _post_features(feature_as_json):
-    resource_base_url = _get_resource_url('features')
+def _post_features(api_base_url, feature_as_json):
+    resource_base_url = _get_api_resource_url(api_base_url, 'features')
     r = requests.post(resource_base_url, json=feature_as_json)
     if r.status_code == 201:
         logger.info("Successfully uploaded feature(id=" +
@@ -91,8 +94,8 @@ def _post_features(feature_as_json):
         logger.info("Unsuccessful: " + r.content)
 
 
-def _get_organization(organization_name):
-    resource_base_url = _get_resource_url('organizations')
+def _get_organization(api_base_url, organization_name):
+    resource_base_url = _get_api_resource_url(api_base_url, 'organizations')
     resource_payload = _get_regex_payload("name", organization_name)
     r = requests.get(resource_base_url, params=resource_payload)
     if r.status_code == 200:
@@ -102,18 +105,13 @@ def _get_organization(organization_name):
     return None
 
 
-def _get_resource_url(resource_name):
-    return API_BASE_URL_FORMAT.format(resource_name)
-
-
-# TODO - Unsafe. (Probably) Need to sanitize, but lazymode atm.
 def _get_regex_payload(field, field_query):
     regex_payload = {field: {'$regex': ".*" + field_query + ".*"}}
     return {'where': json.dumps(regex_payload)}
 
 
 if __name__ == '__main__':
-    import_features()
+    import_park_features()
 
 # vim: fenc=utf-8
 # vim: filetype=python
