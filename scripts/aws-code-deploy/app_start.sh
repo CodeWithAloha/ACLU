09f8ff0b2ee81d6cfa0ca27fd134bdbc1b14f3b0
Pipe Martina <felimartina@outlook.com>
2018-03-03 13:19:08 -1000

    DEVOPS - Added -d flag to docker

--- scripts/aws-code-deploy/app_start.sh
+++ scripts/aws-code-deploy/app_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /var/project-aclu/
-docker run -p 50050:50050 --name "aclu-api" "705750910119.dkr.ecr.us-west-2.amazonaws.com/aclu:latest"
+docker run -p 50050:50050 -d --name "aclu-api" "705750910119.dkr.ecr.us-west-2.amazonaws.com/aclu:latest"
