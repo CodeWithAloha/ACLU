549b59c8a14c4acf1bd2efed26bdb8c4e5031f91
Felipe Martina <felimartina@outlook.com>
2018-04-18 19:29:59 -1000

    Pointed fronent dev environment to api.aclu.codeforhawaii.org

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  ACLU_API_BASE_URL: '"http://localhost:50050"'
+  ACLU_API_BASE_URL: '"http://api.aclu.codeforhawaii.org"'
 })
