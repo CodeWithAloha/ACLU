75500cd3865c4490123819f5511323dafa5af1b1
Felipe Martina <felimartina@outlook.com>
2018-08-15 19:01:05 -1000

    Created project using vue-cli. Added material design

---
+++ frontend-v2/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import App from './App.vue'
+import router from './router'
+
+Vue.use(VueMaterial)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
