2c8894953d0d6bc9caac065c3f233aeaa8ddf13c
Felipe Martina <felimartina@outlook.com>
2018-09-22 18:50:18 -1000

    Added

--- frontend-v2/src/views/Home.vue
+++ frontend-v2/src/views/Home.vue
@@ -1,23 +1,36 @@
 <template>
-  <div class="page-container md-layout-column">
-    <NavBarComponent></NavBarComponent>
-		<md-content>
-			<MapComponent></MapComponent>
-    </md-content>
-  </div>
+	<div>
+		<Loading :loading="loading"></Loading>
+		<div class="page-container md-layout-column">
+			<NavBarComponent></NavBarComponent>
+			<md-content>
+				<MapComponent></MapComponent>
+			</md-content>
+		</div>
+	</div>
 </template>
 
 <script>
+import Loading from '@/components/Loading.vue'
 import MapComponent from '@/components/MapComponent.vue'
 import NavBarComponent from '@/components/NavBarComponent.vue'
+
 export default {
   name: 'Home',
   components: {
 		MapComponent,
-		NavBarComponent
+		NavBarComponent,
+		Loading
 	},
 	data: () => ({
-  })
+		loading: true
+	}),
+	mounted: function () {
+		// Example of how to use "loading" component
+		setTimeout(() => {
+			this.loading = false
+		}, 3000);
+	}
 }
 </script>
 <style scoped>
