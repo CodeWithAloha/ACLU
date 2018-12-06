4e2749a39154241dbd1bfec712ec648dba033802
Felipe Martina <felimartina@outlook.com>
2018-12-06 11:36:00 -1000

    Troubleshooting map styles after centering on user location[B

--- frontend-v2/src/components/Map.vue
+++ frontend-v2/src/components/Map.vue
@@ -3,54 +3,55 @@
     <mapbox
     @map-load="onMapLoaded"
     :accessToken="mapboxToken"
-    :mapOptions="mapOptions"></mapbox>
+    :map-options="{
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [-74.50, 40],
+      zoom: 9
+    }"></mapbox>
+    <!-- Can't bind options to a Vue DataObject because it breaks mapbox -->
   </div>
 </template>
 
 <script>
-import Mapbox from 'mapbox-gl-vue'
-import Constants from '@/services/constants'
-import Geolocation from '@/services/geolocation'
+import Mapbox from "mapbox-gl-vue";
+import Constants from "@/services/constants";
+import Geolocation from "@/services/geolocation";
 
 export default {
-  name: 'Map',
+  name: "Map",
   components: {
     Mapbox
   },
-  data: function () {
+  data: function() {
     return {
-			mapRef: null,
+      map: {},
       mapboxToken: process.env.VUE_APP_MAPBOX_TOKEN,
-      mapOptions: {
-        style: 'mapbox://styles/mapbox/light-v9',
-        center: [
-          Constants.Map.Defaults.Longitude,
-          Constants.Map.Defaults.Latitude
-        ],
-        zoom: Constants.Map.Defaults.Zoom
-      }
-    }
+      style: Constants.Map.Defaults.Style,
+      centerLon: Constants.Map.Defaults.Longitude,
+      centerLat: Constants.Map.Defaults.Latitude,
+      zoom: Constants.Map.Defaults.Zoom
+    };
   },
   methods: {
-    onMapLoaded: function (map) {
-			this.$emit('mapLoaded')
-			this.mapRef = map
-		},
-		centerOnUserLocation: async function() {
-			try {
-				const position = await Geolocation.getCurrentPosition()
-				// this.$store.commit("locationFound", pos.coords);
-				// TODO: store location in use`r session?
-				map.flyTo({
-					center: [pos.coords.longitude, pos.coords.latitude],
-					zoom: 13
-				});
-			} catch (error) {
-				console.log(error)
-			}
-		}
-	}
-}
+    onMapLoaded: async function(map) {
+      this.$emit("mapLoaded");
+      this.map = map;
+      await this.centerAtUserLocation();
+    },
+    centerAtUserLocation: async function() {
+      try {
+        // const pos = await Geolocation.getCurrentPosition();
+        this.map.flyTo({
+          // center: [pos.coords.longitude, pos.coords.latitude],
+          zoom: 13
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>
--- frontend-v2/src/services/constants.js
+++ frontend-v2/src/services/constants.js
@@ -1,7 +1,10 @@
-export const Map = {
-  Defaults: {
-    Latitude: 21.441922,
-    Longitude: -158.000072,
-    Zoom: 10
+module.exports = {
+  Map: {
+    Defaults: {
+      Latitude: 21.441922,
+      Longitude: -158.000072,
+      Zoom: 10,
+      Style: 'mapbox://styles/mapbox/streets-v9' // 'mapbox://styles/mapbox/light-v9'
+    }
   }
 }
