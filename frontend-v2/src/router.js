2e8efbe6ce41dc3f6ca20b410f7c4438d6c2f559
ardeaf <ardeaf@gmail.com>
2019-03-06 19:54:03 -1000

    Added basic about page

--- frontend-v2/src/router.js
+++ frontend-v2/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: About
     },
     {
       path: '/report-feedback',
---
+++ frontend-v2/src/views/About.vue
@@ -0,0 +1,90 @@
+<template>
+    <div id="about-page">
+        <div id="about-page-content">
+            <h1>About Us</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vestibulum vestibulum urna nec maximus.
+                Proin
+                vulputate pharetra tristique. Integer mollis augue sed lacus accumsan euismod eu lobortis mauris. In
+                porta
+                est at arcu laoreet dignissim. Morbi eu lectus nec orci aliquet accumsan. Phasellus euismod consectetur
+                tortor, et finibus quam. Quisque sit amet dignissim nunc, id ultricies lorem. Pellentesque rutrum, lacus
+                sed
+                aliquam dictum, odio enim molestie libero, sed suscipit tortor ante at purus. Nullam elit sem, egestas
+                viverra orci vitae, imperdiet aliquam nulla. Nunc elementum sem sit amet sem pretium, a volutpat ligula
+                tempus. Aliquam molestie metus auctor lacus placerat commodo. Cras eget purus hendrerit nibh egestas
+                imperdiet sit amet eget leo. Vestibulum ornare a arcu sed vulputate.
+            </p>
+            <p>
+                In vel eros sed diam ultricies lacinia non ac erat. Vestibulum non eros nisi. Vestibulum at dolor at dui
+                posuere dictum ut id arcu. Proin elementum, mi a consectetur elementum, sapien elit maximus enim, id
+                porttitor mauris justo sit amet lacus. Duis elementum auctor nisl rhoncus auctor. Praesent et ante ac
+                elit
+                tempor imperdiet id a ex. Cras porttitor ex ultricies purus posuere, vel scelerisque felis tempor.
+                Integer
+                quis egestas turpis. Fusce condimentum orci enim, ut aliquam magna varius a. In augue est, lobortis
+                vitae
+                hendrerit vel, tincidunt fermentum erat. Nulla gravida odio mi, quis facilisis ante scelerisque nec.
+                Cras
+                facilisis justo quis auctor dictum. Phasellus efficitur neque eu tellus viverra venenatis.
+            </p>
+            <p>
+                Aliquam erat volutpat. Fusce sed purus sed est scelerisque mattis. Proin neque urna, aliquam dapibus
+                condimentum eu, vestibulum in augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+                per
+                inceptos himenaeos. Phasellus auctor, ligula eget tincidunt bibendum, nisl felis convallis ex, in congue
+                libero mauris at libero. Sed eget metus lobortis, pharetra augue eu, tristique nulla. Sed gravida massa
+                ut
+                hendrerit ullamcorper. Ut quis tempor massa. Vivamus vitae urna porta, dictum nisl quis, eleifend metus.
+                Sed
+                et metus laoreet, facilisis ante pharetra, dictum diam. Nunc semper egestas nibh, vitae condimentum
+                lacus
+                cursus a. Proin eget nisl magna. Mauris nec purus pretium, faucibus ex et, varius lacus. Proin tincidunt
+                sit
+                amet nisl eu varius.
+            </p>
+            <p>
+                Aenean pulvinar sit amet ante ut blandit. Fusce viverra libero lacus, quis imperdiet ex cursus ac. Morbi
+                egestas ante odio, sit amet tempor arcu aliquam eget. Vivamus vehicula ornare lectus non placerat.
+                Aliquam
+                erat volutpat. Fusce consectetur tincidunt mi, nec imperdiet magna sodales quis. Nulla velit nisi,
+                consectetur quis diam sit amet, feugiat gravida tortor. Aliquam ut elit in mauris efficitur fermentum.
+            </p>
+            <p>
+                Phasellus blandit mattis odio, nec viverra leo tempor eu. Nullam eget neque at lectus dapibus sagittis.
+                In
+                id dolor mauris. Nullam suscipit magna ut dignissim faucibus. Nunc et nisl nec justo semper consectetur
+                in
+                vitae felis. Donec posuere nunc id magna egestas condimentum. Etiam in nisi libero. Vestibulum eget enim
+                a
+                nisl feugiat luctus. Fusce nibh neque, pellentesque ut metus at, gravida interdum nunc. Aenean congue
+                diam
+                in porttitor vestibulum. Sed vitae orci ex. Quisque vulputate augue eget cursus tincidunt. Curabitur
+                congue
+                vehicula eros, ut elementum libero. In vel ligula convallis, laoreet neque a, vestibulum elit. Sed
+                luctus et
+                nulla non tincidunt. Fusce condimentum cursus nibh.
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+    #about-page {
+        background-color: #fafafa;
+        color: rgba(1, 1, 1, 0.95);
+        text-align: center;
+        align-content: center;
+    }
+    #about-page-content {
+        width: 80%;
+        background-color: #fafafa;
+        display: inline-block;
+    }
+</style>
