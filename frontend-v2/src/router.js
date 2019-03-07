efe43aa36017793503d5a68d83f53ee12e312b74
Wyatt Arent <hello@wyatt.engineer>
2019-03-06 19:35:42 -1000

    Add basic 404 foundation

--- frontend-v2/src/router.js
+++ frontend-v2/src/router.js
@@ -18,10 +18,12 @@ export default new Router({
     {
       path: '/settings',
       name: 'settings',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/Settings.vue')
+    },
+    {
+      path: '*',
+      name: '404',
+      component: () => import(/* webpackChunkName: "about" */ './views/404.vue')
     }
   ]
 })
---
+++ frontend-v2/src/views/404.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="NotFound">
+    <h1>404</h1>
+    <p class="subtitle">
+      Page not found.
+    </p>
+    <p class="info">
+      The page doesn't exist. Please double check the URL.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '404'
+}
+</script>
+
+<style lang="scss">
+  .NotFound {
+    h1 {
+      font-size: 5rem;
+    }
+    .subtitle {
+      font-size: 2rem;
+    }
+    .info {
+      font-size: 3rem;
+    }
+  }
+</style>
