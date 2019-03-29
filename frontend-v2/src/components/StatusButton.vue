5a09a917d6511c1149d4731c6d4a885df9f90a15
Felipe Martina <felimartina@outlook.com>
2019-03-29 12:32:19 -1000

    Extracted status buttons to a component for better reutilization

---
+++ frontend-v2/src/components/StatusButton.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="chip" v-bind:class="[sizeClass, colorClass]">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatusButton",
+  components: {},
+  props: ["theme", "size", "text"],
+  computed: {
+    colorClass() {
+      switch (this.theme) {
+        case "success":
+          return "success";
+        case "warning":
+          return "warning";
+        case "alert":
+          return "alert";
+        case "primary":
+        default:
+          return "unknown";
+      }
+    },
+    sizeClass() {
+      switch (this.size) {
+        case "large":
+          return "large";
+        default:
+          return "mini";
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.chip {
+  &.mini {
+    width: 45%;
+    padding: 0.2rem 1rem;
+    &::before {
+      height: 10px;
+    }
+  }
+
+  &.large {
+    min-width: 50%;
+    padding: 0.5rem 1rem;
+    font-size: 1.5rem;
+    &::before {
+      width: 15px;
+      height: 15px;
+      margin-right: 1rem;
+    }
+  }
+
+  margin: 0.5rem 0.2rem;
+  box-shadow: -0.05rem 0.15rem 0.2rem rgba(0, 0, 0, 0.25);
+  border-radius: 0.2rem;
+  font-weight: bold;
+
+  &::before {
+    content: "";
+    border-radius: 100%;
+    margin-right: 0.5rem;
+    display: inline-block;
+  }
+
+  &.success {
+    color: #50d076;
+    &::before {
+      background-color: #50d076;
+      border-right: 5px solid #50d076;
+      border-left: 5px solid #50d076;
+    }
+  }
+  &.alert {
+    color: #f0404d;
+    &::before {
+      background-color: #f0404d;
+      border-right: 5px solid #f0404d;
+      border-left: 5px solid #f0404d;
+    }
+  }
+  &.warning {
+    color: #f6c95f;
+    &::before {
+      background-color: #f6c95f;
+      border-right: 5px solid #f6c95f;
+      border-left: 5px solid #f6c95f;
+    }
+  }
+  &.unknown {
+    color: #699bf9;
+    &::before {
+      background-color: #699bf9;
+      border-right: 5px solid #699bf9;
+      border-left: 5px solid #699bf9;
+    }
+  }
+}
+</style>
--- frontend-v2/src/views/Intro.vue
+++ frontend-v2/src/views/Intro.vue
@@ -43,18 +43,10 @@
         The for states of the locations are the following:
       </p>
       <div class="chip-wrap">
-        <div class="chip mini open">
-          Open
-        </div>
-        <div class="chip mini private">
-          Private
-        </div>
-        <div class="chip mini conditional">
-          Conditional
-        </div>
-        <div class="chip mini unknown">
-          Unknown
-        </div>
+        <StatusButton theme="success" text="Open" ></StatusButton>
+        <StatusButton theme="warning" text="Closing Soon" ></StatusButton>
+        <StatusButton theme="alert" text="Restricted" ></StatusButton>
+        <StatusButton theme="primary" text="Unknown" ></StatusButton>
       </div>
     </template>
   </SplitTemplate>
@@ -67,9 +59,7 @@
       logo
     </template>
     <template slot="body">
-      <div class="chip large private">
-        Private
-      </div>
+      <StatusButton theme="alert" size="large" text="Restricted"></StatusButton>
       <p>
         The color red represents an area that is private or closed to to the public.
       </p>
@@ -84,9 +74,7 @@
       logo
     </template>
     <template slot="body">
-      <div class="chip large conditional">
-        Conditional
-      </div>
+      <StatusButton theme="warning" size="large" text="Closing Soon"></StatusButton>
       <p>
         The yellow polygon represents an area in which the status will change soon.
       </p>
@@ -101,9 +89,7 @@
       logo
     </template>
     <template slot="body">
-      <div class="chip large open">
-        Open
-      </div>
+      <StatusButton theme="alert" size="large" text="Restricted"></StatusButton>
       <p>
         The green polygon represents an area that is open to the public.
       </p>
@@ -118,9 +104,7 @@
       logo
     </template>
     <template slot="body">
-      <div class="chip large unknown">
-        Unknown
-      </div>
+      <StatusButton theme="primary" size="large" text="Unknown"></StatusButton>
       <p>
         The blue polygon will be displayed when we are unable to identify the location  or restrictions of an area.
       </p>
@@ -130,6 +114,7 @@
 
 <script>
 import SplitTemplate from '@/views/SplitTemplate.vue'
+import StatusButton from "@/components/StatusButton.vue"
 
 export default {
   name: 'intro',
@@ -153,85 +138,16 @@ export default {
     }
   },
   components: {
-    SplitTemplate
+    SplitTemplate,
+    StatusButton
   }
 }
 </script>
 
 <style lang="scss">
-
 .chip-wrap {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
 }
-
-.chip {
-
-  &.mini {
-    width: 45%;
-    padding: 0.2rem 1rem;
-    &::before {
-      height: 10px;
-    }
-  }
-
-  &.large {
-    min-width: 50%;
-    padding: 0.5rem 1rem;
-    font-size: 1.5rem;
-    &::before {
-      width: 15px;
-      height: 15px;
-      margin-right: 1rem;
-    }
-  }
-
-  margin: 0.5rem 0.2rem;
-  box-shadow: -0.05rem 0.15rem 0.2rem rgba(0, 0, 0, 0.25);
-  border-radius: 0.2rem;
-  font-weight: bold;
-
-  &::before {
-    content: "";
-    border-radius: 100%;
-    margin-right: 0.5rem;
-    display: inline-block;
-  }
-
-  &.open {
-    color: #50d076;
-    &::before {
-      background-color: #50d076;
-      border-right: 5px solid #50d076;
-      border-left: 5px solid #50d076;
-    }
-  }
-  &.private {
-    color: #f0404d;
-    &::before {
-      background-color: #f0404d;
-      border-right: 5px solid #f0404d;
-      border-left: 5px solid #f0404d;
-    }
-  }
-  &.conditional {
-    color: #f6c95f;
-    &::before {
-      background-color: #f6c95f;
-      border-right: 5px solid #f6c95f;
-      border-left: 5px solid #f6c95f;
-    }
-  }
-  &.unknown {
-    color: #699bf9;
-    &::before {
-      background-color: #699bf9;
-      border-right: 5px solid #699bf9;
-      border-left: 5px solid #699bf9;
-    }
-  }
-
-}
-
 </style>
