73ed5e786246c64e620e6bbfd200bfd9499fa603
John Zhou <john.km.zhou@gmail.com>
2019-03-30 14:14:23 -1000

    Fix lint issues.

--- frontend-v2/src/components/Map.vue
+++ frontend-v2/src/components/Map.vue
@@ -16,37 +16,37 @@
 </template>
 
 <script>
-import Loading from "@/components/Loading";
-import StatusButton from "@/components/StatusButton";
-import Mapbox from "mapbox-gl-vue";
-import { Colors, Map, Settings, OpenStatus } from "@/services/constants";
-import MapboxGeocoder from "mapbox-gl-geocoder";
-import "mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
-import FeatureService from "@/services/features";
+import Loading from '@/components/Loading'
+import StatusButton from '@/components/StatusButton'
+import Mapbox from 'mapbox-gl-vue'
+import { Colors, Map, Settings, OpenStatus } from '@/services/constants'
+import MapboxGeocoder from 'mapbox-gl-geocoder'
+import 'mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+import FeatureService from '@/services/features'
 import {
   MapBoxColorExpression,
   MapBoxPatternExpression,
   Patterns
-} from "@/utils/mapHelper";
+} from '@/utils/mapHelper'
 /**
  *  We have to keep the map reference outside vue 'data' object
  *  otherwise the mapbox styles break
  */
-let mapRef = {};
-let geocoder;
-let currentMapId; // store a temp id for current map status (work around since layer removal does not work properly)
+let mapRef = {}
+let geocoder
+let currentMapId // store a temp id for current map status (work around since layer removal does not work properly)
 export default {
-  name: "Map",
+  name: 'Map',
   components: {
     Loading,
     Mapbox,
     StatusButton
   },
-  data: function() {
+  data: function () {
     return {
       mapboxToken: Settings.MapBoxToken,
       mapOptions: {
-        container: "map",
+        container: 'map',
         style: Map.Defaults.Style,
         center: [Map.Defaults.Longitude, Map.Defaults.Latitude],
         zoom: Map.Defaults.Zoom
@@ -54,7 +54,7 @@ export default {
       loading: true,
       geolocateControl: {
         show: true,
-        position: "top-left",
+        position: 'top-left',
         options: {
           trackUserLocation: false,
           positionOptions: {
@@ -63,182 +63,182 @@ export default {
         }
       },
       selectedFeature: null
-    };
+    }
   },
   computed: {
-    selectedFeatureStatus() {
-      if (!this.selectedFeature) return;
+    selectedFeatureStatus () {
+      if (!this.selectedFeature) return
       switch (this.selectedFeature.properties.condition) {
         case OpenStatus.Open:
           return {
-            theme: "success",
-            text: "Permitted"
-          };
+            theme: 'success',
+            text: 'Permitted'
+          }
         case OpenStatus.ClosingSoon:
           return {
-            theme: "warning",
-            text: "Closing Soon"
-          };
+            theme: 'warning',
+            text: 'Closing Soon'
+          }
         case OpenStatus.Closed:
           return {
-            theme: "alert",
-            text: "Restricted"
-          };
+            theme: 'alert',
+            text: 'Restricted'
+          }
         case OpenStatus.Unknown:
         default:
           return {
-            theme: "primary",
-            text: "Unknown"
-          };
+            theme: 'primary',
+            text: 'Unknown'
+          }
       }
     }
   },
-  mounted() {},
+  mounted () {},
   methods: {
-    async onMapLoaded(map) {
+    async onMapLoaded (map) {
       try {
-        mapRef = map;
-        await this.loadMapboxWidgets(mapRef);
-        this.$emit("mapLoaded");
-        this.loading = false;
+        mapRef = map
+        await this.loadMapboxWidgets(mapRef)
+        this.$emit('mapLoaded')
+        this.loading = false
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     },
-    onUserIsGeolocated(geolocateControl, pos) {
-      this.loadFeatures(pos.coords.latitude, pos.coords.longitude);
+    onUserIsGeolocated (geolocateControl, pos) {
+      this.loadFeatures(pos.coords.latitude, pos.coords.longitude)
     },
-    async loadMapboxWidgets(map) {
+    async loadMapboxWidgets (map) {
       // Geocoder (Search Bar)
       // TODO: It'd be nice if we can make this its own controller
       // Limit results to hawaii only
-      const bboxHawaii = [-160.3, 16.7, -151.8, 23.3];
+      const bboxHawaii = [-160.3, 16.7, -151.8, 23.3]
       geocoder = new MapboxGeocoder({
         accessToken: Settings.MapBoxToken,
         bbox: bboxHawaii
-      });
-      geocoder.on("result", ev => {
-        const [lon, lat] = ev.result.geometry.coordinates;
-        this.loadFeatures(lat, lon);
-      });
-      document.getElementById("geocoder").appendChild(geocoder.onAdd(mapRef));
+      })
+      geocoder.on('result', ev => {
+        const [lon, lat] = ev.result.geometry.coordinates
+        this.loadFeatures(lat, lon)
+      })
+      document.getElementById('geocoder').appendChild(geocoder.onAdd(mapRef))
 
       // Load patterns for layers
       await Promise.all([
         new Promise((resolve, reject) => {
           mapRef.loadImage(Patterns.GreenPattern, (err, img) => {
-            if (err) reject(err);
-            mapRef.addImage("GreenPattern", img);
-            resolve();
-          });
+            if (err) reject(err)
+            mapRef.addImage('GreenPattern', img)
+            resolve()
+          })
         }),
         new Promise((resolve, reject) => {
           mapRef.loadImage(Patterns.RedPattern, (err, img) => {
-            if (err) reject(err);
-            mapRef.addImage("RedPattern", img);
-            resolve();
-          });
+            if (err) reject(err)
+            mapRef.addImage('RedPattern', img)
+            resolve()
+          })
         }),
         new Promise((resolve, reject) => {
           mapRef.loadImage(Patterns.YellowPattern, (err, img) => {
-            if (err) reject(err);
-            mapRef.addImage("YellowPattern", img);
-            resolve();
-          });
+            if (err) reject(err)
+            mapRef.addImage('YellowPattern', img)
+            resolve()
+          })
         })
-      ]);
+      ])
     },
-    async loadFeatures(lat, lon) {
-      this.loading = true;
+    async loadFeatures (lat, lon) {
+      this.loading = true
       // TODO: Yield features instead of return whole array (since it requires multiple requests)
-      const features = await FeatureService.getFeaturesNearBy(lat, lon);
-      const source = [];
+      const features = await FeatureService.getFeaturesNearBy(lat, lon)
+      const source = []
       for (const f of features) {
         if (!this.$store.state.renderedFeatures[f._id]) {
-          const geo = f.geojson;
-          geo.properties.condition = await f.getStatus();
-          geo.properties.id = f._id;
-          source.push(geo);
+          const geo = f.geojson
+          geo.properties.condition = await f.getStatus()
+          geo.properties.id = f._id
+          source.push(geo)
           // Keep a "set" of all distinct rendered features so we don't render them twice
-          this.$store.state.renderedFeatures[f._id] = geo;
+          this.$store.state.renderedFeatures[f._id] = geo
         }
       }
       // Create an id for this source and layer (mapbox accepts unique sources and layers only)
       if (mapRef.getSource(currentMapId)) {
         try {
-          mapRef.removeSource(currentMapId);
-          mapRef.removeLayer(`${currentMapId}-pattern`);
-          mapRef.removeLayer(`${currentMapId}-color`);
-          mapRef.removeLayer(`${currentMapId}-selected`);
+          mapRef.removeSource(currentMapId)
+          mapRef.removeLayer(`${currentMapId}-pattern`)
+          mapRef.removeLayer(`${currentMapId}-color`)
+          mapRef.removeLayer(`${currentMapId}-selected`)
         } catch (error) {
           // swallow
-          console.log(error);
+          console.log(error)
         }
       }
 
-      currentMapId = Date.now().toString();
+      currentMapId = Date.now().toString()
       mapRef.addSource(currentMapId, {
-        type: "geojson",
+        type: 'geojson',
         data: {
-          type: "FeatureCollection",
+          type: 'FeatureCollection',
           features: Object.values(this.$store.state.renderedFeatures)
         }
-      });
+      })
 
       mapRef.addLayer({
         id: `${currentMapId}-pattern`,
-        type: "fill",
+        type: 'fill',
         source: currentMapId,
         minzoom: 14,
         paint: {
-          "fill-pattern": MapBoxPatternExpression
+          'fill-pattern': MapBoxPatternExpression
         },
-        filter: ["==", "$type", "Polygon"]
-      });
+        filter: ['==', '$type', 'Polygon']
+      })
 
       mapRef.addLayer({
         id: `${currentMapId}-color`,
-        type: "fill",
+        type: 'fill',
         source: currentMapId,
         paint: {
-          "fill-color": MapBoxColorExpression,
-          "fill-opacity": 0.3,
-          "fill-outline-color": Colors.LayerBorder
+          'fill-color': MapBoxColorExpression,
+          'fill-opacity': 0.3,
+          'fill-outline-color': Colors.LayerBorder
         },
-        filter: ["!in", "id"]
-      });
+        filter: ['!in', 'id']
+      })
 
       mapRef.addLayer({
         id: `${currentMapId}-selected`,
-        type: "fill",
+        type: 'fill',
         source: currentMapId,
         paint: {
-          "fill-color": MapBoxColorExpression,
-          "fill-opacity": 0.8
+          'fill-color': MapBoxColorExpression,
+          'fill-opacity': 0.8
         },
-        filter: ["in", "id"]
-      });
+        filter: ['in', 'id']
+      })
 
-      const selectFeatureClick = function(e) {
-        this.selectedFeature = e.features[0];
+      const selectFeatureClick = function (e) {
+        this.selectedFeature = e.features[0]
         mapRef.setFilter(`${currentMapId}-selected`, [
-          "in",
-          "id",
+          'in',
+          'id',
           this.selectedFeature.properties.id
-        ]);
+        ])
         mapRef.setFilter(`${currentMapId}-color`, [
-          "!in",
-          "id",
+          '!in',
+          'id',
           this.selectedFeature.properties.id
-        ]);
-      };
+        ])
+      }
       // Select feature on click
-      mapRef.on("click", `${currentMapId}-color`, selectFeatureClick);
-      mapRef.on("click", `${currentMapId}-pattern`, selectFeatureClick);
-      this.loading = false;
+      mapRef.on('click', `${currentMapId}-color`, selectFeatureClick)
+      mapRef.on('click', `${currentMapId}-pattern`, selectFeatureClick)
+      this.loading = false
     }
   }
-};
+}
 </script>
 
 <style lang='css'>
