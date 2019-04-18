81fef038890169af75386c190aede49ed4515bfe
Suchandra Thapa <sthapa@ci.uchicago.edu>
2019-04-17 19:17:45 -1000

    Add google form embed

--- frontend-v2/src/views/ReportFeedback.vue
+++ frontend-v2/src/views/ReportFeedback.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="report-feedback">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <p>Report Feedback screen</p>
+    <h1>Report Feedback</h1>
+    <p>Thank you for reporting feedback, this helps to improve Holo Holo for all users</p>
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd7ITmDa3qpooCOSuEMRYKCGBCNWJSEbUtIO7Yop-OQ9TUtUQ/viewform?embedded=true"
+            width="100%"
+            height="100%"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            style="height: 600px">
+      Loading...
+    </iframe>
   </div>
 </template>
 
