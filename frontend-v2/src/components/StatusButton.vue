1a22978af129db77c99f32996209ae37c3a24098
John Zhou <john.km.zhou@gmail.com>
2019-07-17 19:06:11 -1000

    Refactor status button into an actual button.

--- frontend-v2/src/components/StatusButton.vue
+++ frontend-v2/src/components/StatusButton.vue
@@ -1,21 +1,15 @@
 <template>
-  <!-- <router-link to="/restriction-details" class="chip" v-bind:class="[sizeClass, renderProperties.theme]">
-    {{ renderProperties.text }}
-  </router-link>-->
-  <a
-    href="#"
-    class="chip"
-    v-bind:class="[sizeClass, renderProperties.theme]"
-    @click="onClick"
-  >{{ renderProperties.text }}</a>
+  <md-button
+    class="md-raised status"
+    v-bind:class="[renderProperties.theme]"
+  >{{ renderProperties.text }}</md-button>
 </template>
 
 <script>
 import { OpenStatus } from '@/services/constants'
 export default {
   name: 'StatusButton',
-  components: {},
-  props: ['status', 'size'],
+  props: ['status'],
   computed: {
     renderProperties () {
       switch (this.status) {
@@ -41,92 +35,28 @@ export default {
             text: 'Unknown'
           }
       }
-    },
-    sizeClass () {
-      switch (this.size) {
-        case 'large':
-          return 'large'
-        default:
-          return 'mini'
-      }
-    }
-  },
-  methods: {
-    onClick () {
-      this.$emit('click')
     }
   }
 }
 </script>
 
 <style lang="scss">
-.chip {
-  &.mini {
-    width: 45%;
-    padding: 0.2rem 1rem;
-    &::before {
-      height: 10px;
-    }
-  }
-
-  &.large {
-    min-width: 50%;
-    padding: 0.5rem 1rem;
-    font-size: 1.5rem;
-    &::before {
-      width: 15px;
-      height: 15px;
-      margin-right: 0.5rem;
-    }
-  }
-
-  margin: 0.5rem 0.2rem;
-  background-color: #ffffff;
-  box-shadow: -0.05rem 0.15rem 0.2rem rgba(0, 0, 0, 0.25);
-  border-radius: 0.2rem;
+.md-button.md-raised.status {
   font-weight: bold;
-  text-decoration: none !important;
-  &.no-gutter {
-    margin: 0rem 0rem;
-  }
-  &::before {
-    content: "";
-    border-radius: 100%;
-    margin-right: 0.5rem;
-    display: inline-block;
-  }
+  padding-left: 1rem;
+  padding-right: 1rem;
 
   &.success {
-    color: #50d076 !important;
-    &::before {
-      background-color: #50d076;
-      width: 15px;
-      height: 15px;
-    }
+    color: #50d076;
   }
   &.alert {
-    color: #f0404d !important;
-    &::before {
-      background-color: #f0404d;
-      width: 15px;
-      height: 15px;
-    }
+    color: #f0404d;
   }
   &.warning {
-    color: #f6c95f !important;
-    &::before {
-      background-color: #f6c95f;
-      width: 15px;
-      height: 15px;
-    }
+    color: #f6c95f;
   }
   &.primary {
-    color: #699bf9 !important;
-    &::before {
-      background-color: #699bf9;
-      width: 15px;
-      height: 15px;
-    }
+    color: #699bf9;
   }
 }
 </style>
