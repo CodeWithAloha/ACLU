f79a2c198dbccd39dc22104f643023193f2d7316
Suchandra Thapa <suchandra@gmail.com>
2020-03-22 01:27:40 -1000

    improve formatting

--- backend/data/tmk/20170713/geojson_chunker.py
+++ backend/data/tmk/20170713/geojson_chunker.py
@@ -15,42 +15,42 @@ import os
 @click.option('--destination', default="./tmp", help="Destination folder for files")
 @click.option('--count_per_file', default=10000, help="Number of polygons per file")
 def split(source, destination, count_per_file):
-    file = open(source, 'r')
+  file = open(source, 'r')
+  line = file.readline()
+  header = ''
+  while is_not_a_feature(line):
+    if not line.startswith('"crs'):
+      header += line
     line = file.readline()
-    header = ''
-    while is_not_a_feature(line):
-        if not line.startswith('"crs'):
-          header += line
-        line = file.readline()
-    number=0
-    while True:
-        output=open("%s/%s.geojson" %(destination, number),'w')
-        output.write(header)
-        number+=1
-        feature=file.readline()
-        # Stop when we find the end of the file
+  number = 0
+  while True:
+    output = open("%s/%s.geojson" % (destination, number), 'w')
+    output.write(header)
+    number += 1
+    feature = file.readline()
+    # Stop when we find the end of the file
+    if is_not_a_feature(feature):
+      break
+    else:
+      for i in range(count_per_file):
         if is_not_a_feature(feature):
-            break
+          print(feature)
+          if i < count_per_file - 1:
+            print(i)
+            print(count_per_file)
+            # remove last ',' on features
+            output.seek(-1, os.SEEK_END)
+            output.truncate()
+          output.write("]}")
+          output.close()
+          sys.exit("Done!")
         else:
-            for i in range(count_per_file):
-                if is_not_a_feature(feature):
-                    print(feature)
-                    if i < count_per_file -1:
-                        print(i)
-                        print(count_per_file)
-                        # remove last ',' on features
-                        output.seek(-1, os.SEEK_END)
-                        output.truncate()
-                    output.write("]}")
-                    output.close()
-                    sys.exit("Done!")
-                else:
-                    if feature == '}':
-                        print("wrong!!!")
-                    output.write(feature)
-                    feature=file.readline()
-            output.write("]}")
-            output.close()
+          if feature == '}':
+            print("wrong!!!")
+          output.write(feature)
+          feature = file.readline()
+      output.write("]}")
+      output.close()
 
 
 def is_not_a_feature(str):
@@ -59,4 +59,4 @@ def is_not_a_feature(str):
 
 
 if __name__ == "__main__":
-    split()
+  split()
